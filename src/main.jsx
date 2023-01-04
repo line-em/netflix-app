73e4e1a6b44032f171c6620137c2ebf13b8e015c
Line-em <alineemily@hotmail.com>
2023-01-04 12:51:27 +0000

    add firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+import { FirebaseContext } from "./context/firebase";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const firebaseConfig = {
+	apiKey: "AIzaSyDEoGbmTFiRwRjXAh6zbj_1cUkURFimOCA",
+	authDomain: "netflix-scrimba-5c3e6.firebaseapp.com",
+	projectId: "netflix-scrimba-5c3e6",
+	storageBucket: "netflix-scrimba-5c3e6.appspot.com",
+	messagingSenderId: "814214750782",
+	appId: "1:814214750782:web:a9c5124763f5cc533e676f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<FirebaseContext.Provider value={{ firebase: app }}>
+		<App />
+	</FirebaseContext.Provider>
+);
