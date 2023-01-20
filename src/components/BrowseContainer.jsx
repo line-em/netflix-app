73118c17e4adca2e6163a2a2f41c3eae8bc71602
Line-em <alineemily@hotmail.com>
2023-01-20 18:07:41 +0000

    header content singout

--- src/components/BrowseContainer.jsx
+++ src/components/BrowseContainer.jsx
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Header from "./header/Header";
 import FooterContainer from "./footer/FooterContainer";
 import { FirebaseContext } from "../context/firebase";
 import ProfileContainer from "./profiles/ProfileContainer";
 
 const BrowseContainer = () => {
+	const [category, setCategory] = useState("series");
 	const [profile, setProfile] = useState({});
+	const [loading, setLoading] = useState(true);
+
+	const { firebase } = useContext(FirebaseContext);
 
 	const user = {
 		displayName: "Karl",
 		photoURL: "1"
 	};
+
 	return profile.displayName ? (
 		<>
-			<p>Browse Container</p>
+			<Header src="joker1" dontShowOnSmallViewPort>
+				<Header.Frame>
+					<Header.Group>
+						<Header.Logo to="/" src="/images/misc/logo.svg" alt="Netflix" />
+						<Header.Link
+							active={category === "series" ? "true" : "false"}
+							onClick={() => setCategory("series")}
+						>
+							Series
+						</Header.Link>
+						<Header.Link
+							active={category === "films" ? "true" : "false"}
+							onClick={() => setCategory("films")}
+						>
+							Films
+						</Header.Link>
+					</Header.Group>
+				</Header.Frame>
+			</Header>
 			<FooterContainer />
 		</>
 	) : (
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
-import { Background, Container, Logo, ButtonLink } from "./styles";
+import { Background, Container, Logo, ButtonLink, Group, Link, Text } from "./styles";
 
 const Header = ({ bg = true, children, ...props }) => {
 	return bg ? <Background {...props}>{children}</Background> : children;
@@ -10,6 +10,10 @@ const HeaderFrame = ({ children, ...props }) => {
 	return <Container {...props}>{children}</Container>;
 };
 
+const HeaderGroup = ({ children, ...props }) => {
+	return <Group {...props}>{children}</Group>;
+};
+
 const HeaderLogo = ({ to, ...props }) => {
 	return (
 		<ReactRouterLink to={to}>
@@ -17,11 +21,23 @@ const HeaderLogo = ({ to, ...props }) => {
 		</ReactRouterLink>
 	);
 };
+
+const HeaderText = ({ children, ...props }) => {
+	return <Text {...props}>{children}</Text>;
+};
+
 const HeaderButtonLink = ({ children, ...props }) => {
 	return <ButtonLink {...props}>{children}</ButtonLink>;
 };
 
+const HeaderLink = ({ children, ...props }) => {
+	return <Link {...props}>{children}</Link>;
+};
+
 Header.Frame = HeaderFrame;
+Header.Text = HeaderText;
+Header.Group = HeaderGroup;
 Header.Logo = HeaderLogo;
 Header.ButtonLink = HeaderButtonLink;
+Header.Link = HeaderLink;
 export default Header;
