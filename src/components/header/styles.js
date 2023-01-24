b0892fd2f53b645e28840bc6f1995dab1b540011
Line-em <alineemily@hotmail.com>
2023-01-24 12:36:05 +0000

    finishing the input styles

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -144,4 +144,15 @@ export const SearchIcon = styled.div`
 	}
 `;
 
-export const SearchInput = styled.input``;
+export const SearchInput = styled.input`
+	background-color: #44444459;
+	color: white;
+	border: 1px solid white;
+	transition: width 0.5s;
+	height: 30px;
+	font-size: 14px;
+	margin-left: ${({ active }) => (active === true ? "10px" : "0")};
+	padding: ${({ active }) => (active === true ? "0 10px" : "0")};
+	opacity: ${({ active }) => (active === true ? "1" : "0")};
+	width: ${({ active }) => (active === true ? "200px" : "0px")};
+`;
